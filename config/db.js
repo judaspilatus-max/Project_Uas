32c07fd5a55f9d4ec9ab99c9f9fb6b121c79e821
Andre Ferdi <email_andre@gmail.com>
2026-07-05 21:11:46 +0700

    fix: koneksi database final

--- config/db.js
+++ config/db.js
@@ -1,23 +1,14 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'hayabusa.proxy.rlwy.net',
-    user: 'root',
+    host: 'hayabusa.proxy.rlwy.net', 
+    user: 'root', 
     password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet',
-    database: 'railway',
-    port: 46715,
+    database: 'railway', 
+    port: 46715, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
-
-db.getConnection((err, conn) => {
-    if (err) {
-        console.error('DATABASE GAGAL:', err);
-    } else {
-        console.log('DATABASE TERKONEKSI!');
-        conn.release();
-    }
 });
 
 module.exports = db.promise();
