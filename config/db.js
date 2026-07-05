3b2bf482642623c14eb68059c07478e310d9588a
Andre Ferdi <email_andre@gmail.com>
2026-07-05 20:57:23 +0700

    fix: hardcode database tanpa env

--- config/db.js
+++ config/db.js
@@ -2,22 +2,13 @@ const mysql = require('mysql2');
 
 const db = mysql.createPool({
     host: 'hayabusa.proxy.rlwy.net',
-    user: 'root',
-    password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet',
+    user: 'root', // Pastikan user-nya memang 'root' sesuai dashboard
+    password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet', // Pastikan ini tepat
     database: 'railway',
     port: 46715,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
-
-db.getConnection((err, connection) => {
-    if (err) {
-        console.error('ERROR DATABASE:', err);
-    } else {
-        console.log('Koneksi Database BERHASIL!');
-        connection.release();
-    }
 });
 
 module.exports = db.promise();
