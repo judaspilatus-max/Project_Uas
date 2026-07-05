5548a294832d052f868ac62cb4b7894a27971b93
Andre Ferdi <email_andre@gmail.com>
2026-07-05 20:59:58 +0700

    fix: hardcode database tanpa env

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'hayabusa.proxy.rlwy.net',
-    user: 'root', // Pastikan user-nya memang 'root' sesuai dashboard
-    password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet', // Pastikan ini tepat
-    database: 'railway',
-    port: 46715,
+    // Railway akan mengisi nilai ini secara otomatis dari fitur Reference 
+    // yang sudah kamu atur di tab Variables
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
