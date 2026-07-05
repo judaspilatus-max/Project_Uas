b838ae347a5e5a3622574925d9340fd114e146d3
Andre Ferdi <email_andre@gmail.com>
2026-07-05 20:52:54 +0700

    fix: hardcode final database

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    host: 'hayabusa.proxy.rlwy.net',
+    user: 'root',
+    password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet',
+    database: 'railway',
+    port: 46715,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
