be248792a24284fa90bc53545c0965e2522ea255
Andre Ferdi <email_andre@gmail.com>
2026-07-05 21:18:44 +0700

    fix: update mysql connection to use environment variables for railway deployment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'hayabusa.proxy.rlwy.net', 
-    user: 'root', 
-    password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet',
-    database: 'railway', 
-    port: 46715, 
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT || 3306, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
