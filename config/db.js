82d5f1f0513ff8093835e017eea7b45106caa835
Andre Ferdi <email_andre@gmail.com>
2026-07-05 21:05:27 +0700

    fix: bypass env, hardcode database

--- config/db.js
+++ config/db.js
@@ -1,26 +1,14 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    // Railway akan mengisi nilai ini secara otomatis dari fitur Reference 
-    // yang sudah kamu atur di tab Variables
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.MYSQLPORT || 3306,
+    host: 'hayabusa.proxy.rlwy.net',
+    user: 'root',
+    password: 'NRBbtcuBSyycKYsirloqiEBZEYacPFet',
+    database: 'railway',
+    port: 46715,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
-
-db.getConnection((err, connection) => {
-    if (err) {
-        // Ganti console.error menjadi ini agar Railway mencatat error lengkap
-        console.error('DATABASE ERROR LENGKAP:', err); 
-        return;
-    }
-    console.log('Koneksi Database BERHASIL!');
-    connection.release();
 });
 
 module.exports = db.promise();
